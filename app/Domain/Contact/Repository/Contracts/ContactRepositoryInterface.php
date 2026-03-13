c280353eece62bf886808b13df30e881df97d083
Elivelton <eliveltongsoficial@gmail.com>
2026-03-13 16:52:24 -0300

    Refactoring

--- app/Domain/Contact/Repository/Contracts/ContactRepositoryInterface.php
+++ app/Domain/Contact/Repository/Contracts/ContactRepositoryInterface.php
@@ -3,15 +3,5 @@
 namespace App\Domain\Contact\Repository\Contracts;
 
 use App\Models\Contact;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Pagination\LengthAwarePaginator;
 
-interface ContactRepositoryInterface
-{
-    // Functions default
-    public function all(): LengthAwarePaginator;
-    public function show(int $id): ?Model;
-    public function create(array $data): Model;
-    public function update(int $id, array $data): ?Model;
-    public function delete(int $id): bool;
-}
+interface ContactRepositoryInterface {}
